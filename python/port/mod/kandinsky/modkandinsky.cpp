3d626696b61f733096bcdee15a312d7278929ab4
TomVdt <tomtommy135@gmail.com>
2021-02-25 18:17:14 +0100

    Fix bad copy-pasting

--- python/port/mod/kandinsky/modkandinsky.cpp
+++ python/port/mod/kandinsky/modkandinsky.cpp
@@ -5,6 +5,7 @@ extern "C" {
 #include <kandinsky.h>
 #include <ion.h>
 #include "port.h"
+#include <math.h>
 
 
 static mp_obj_t TupleForKDColor(KDColor c) {
@@ -91,6 +92,7 @@ mp_obj_t modkandinsky_draw_rect(size_t n_args, const mp_obj_t * args) {
   mp_int_t y = mp_obj_get_int(args[1]);
   mp_int_t width = mp_obj_get_int(args[2]);
   mp_int_t height = mp_obj_get_int(args[3]);
+  mp_int_t stroke = mp_obj_get_int(args[4]);
   if (width < 0) {
     width = -width;
     x = x - width;
